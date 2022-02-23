5b2f61100aad2ffd4fe6eb48c7d3c6c752fbe8ec
coni <46821134+coni@users.noreply.github.com>
2022-02-23 15:26:42 +0000

    center wallpaper

--- dot/.config/dwm/autostart.sh
+++ dot/.config/dwm/autostart.sh
@@ -2,7 +2,7 @@
 
 # add your autostart stuffs here
 # xrdb merge ~/Public/xresources/forest/.Xresources &
-feh --bg-tile $HOME/.config/dwm/wallpaper/default.png &
+feh --bg-tile --bg-center $HOME/.config/dwm/wallpaper/default.png &
 setxkbmap fr
 
 # run bar script and dwm ( do not remove this ) 
