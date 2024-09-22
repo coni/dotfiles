33405273dcbc04ebd2f81f2998976235155d91e5
coni <46821134+coni@users.noreply.github.com>
2024-09-22 16:35:37 +0000

    eww for dwl

---
+++ .config/eww/scripts/dwl.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+if [ "$#" -le "0" ];
+then
+    echo non
+fi
+
+MONITOR=$1
+if [ "$2" == "title" ];
+then
+    while read line;
+    do
+        grep title <<< "$line" > /dev/null
+        if [ "$?" -eq "0" ];
+        then
+            cut -d ' ' -f2- <<< "$line"
+        fi
+    done < <(dwlmsg -o $MONITOR -w -c)
+elif [ "$2" == "workspaces" ];
+then
+    counter=0
+    maxtags=$(dwlmsg -o $MONITOR -g -T)
+    current=0
+    while read yes; do
+        objects=()
+        while read -r line; do
+            if [ "$counter" -ge "$maxtags" ]; then
+                counter=0
+            else
+                parameter=($line)
+                id=${parameter[1]}
+                windows=${parameter[3]}
+                if [ "${parameter[2]}" -eq "1" ];
+                then
+                    current=$id
+                fi
+                objects+=("$(jq -a -c -n --arg id "$id" --arg windows "$windows" '{id: $id, windows: $windows}')")
+                counter=$((counter + 1))
+            fi
+        done < <(dwlmsg -o $MONITOR -g -t)
+        jq --argjson current "$current" --argjson maxtags "$maxtags" -c -M -s '{current: $current, max: $maxtags, workspaces: .}' <<< "${objects[@]}"
+    done < <(dwlmsg -o $MONITOR -w -l)
+elif [[ "$2" == "workspace" && "$#" -ge "3" ]];
+then
+    maxtags=$(dwlmsg -o $MONITOR -g -T)
+    if [[ "$3" -lt "0" || "$3" -ge "$maxtags" ]];
+    then
+        exit 1
+    fi
+    dwlmsg -o $MONITOR -t $3
+fi
