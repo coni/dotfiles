062b9684e517c65f0058bcede5f77c652fcab2c8
coni <46821134+coni@users.noreply.github.com>
2024-11-01 22:00:59 +0100

    update icon for desktop without wifi and battery

--- .config/eww/scripts/network.sh
+++ .config/eww/scripts/network.sh
@@ -5,8 +5,8 @@ do
     [[ "$(nmcli radio wifi)" = "enabled" ]] && airplane=false || airplane=true
 
     nmcli -t -f DEVICE,STATE device | awk -F':' -v airplane="$airplane" '
-    BEGIN { wifi = "false"; eth = "false" }
+    BEGIN { wifi = "\"none\""; eth = "false" }
     /^wl[a-z0-9]+:/ { wifi = ($2 == "connected") ? "true" : "false" }
-    /^enp0s[0-9a-z]*:/ { eth = ($2 == "connected") ? "true" : "false" }
-    END { print "{\"wifi\": " wifi ", \"eth\": " "false" ", \"airplane\": " airplane "}" }'
+    /^enp[0-9]s[0-9a-z]*:/ { eth = ($2 == "connected") ? "true" : "false" }
+    END { print "{\"wifi\": " wifi ", \"eth\": " eth ", \"airplane\": " airplane "}" }'
 done < <(nmcli monitor)
